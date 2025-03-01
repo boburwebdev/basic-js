8baf6e264438a80352f836f001c7f625d333ec33
boburwebdev <boburjon.zokirov.2017@gmail.com>
2025-03-02 00:56:07 +0500

    feat: add solution to mine-sweeper

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,103 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  const newArr = [];
+
+  for (let i = 0; i < matrix.length; i++) {
+    newArr[i] = [...matrix[i]];
+    for (let j = 0; j < matrix[i].length; j++) {
+      let count = 0;
+
+      if (i === 0) {
+        if (matrix[i][j + 1]) {
+          count++;
+        }
+
+        if (matrix[i][j - 1]) {
+          count++;
+        }
+
+        if (matrix[i + 1][j]) {
+          count++;
+        }
+
+        if (matrix[i + 1][j + 1]) {
+          count++;
+        }
+
+        if (matrix[i + 1][j - 1]) {
+          count++;
+        }
+      } else if (i === matrix.length - 1) {
+        if (matrix[i][j + 1]) {
+          count++;
+        }
+
+        if (matrix[i][j - 1]) {
+          count++;
+        }
+
+        if (matrix[i - 1][j]) {
+          count++;
+        }
+
+        if (matrix[i - 1][j + 1]) {
+          count++;
+        }
+
+        if (matrix[i - 1][j - 1]) {
+          count++;
+        }
+      } else {
+        if (matrix[i][j + 1]) {
+          count++;
+        }
+
+        if (matrix[i - 1][j]) {
+          count++;
+        }
+
+        if (matrix[i - 1][j + 1]) {
+          count++;
+        }
+
+        if (matrix[i - 1][j - 1]) {
+          count++;
+        }
+
+        if (matrix[i + 1][j]) {
+          count++;
+        }
+
+        if (matrix[i + 1][j + 1]) {
+          count++;
+        }
+
+        if (matrix[i + 1][j - 1]) {
+          count++;
+        }
+
+        if (j >= 1) {
+
+          if (matrix[i][j - 1]) {
+            count++;
+          }
+
+          if (i < matrix.length - 2) {
+            if (matrix[i + 1][j - 1]) {
+              count++;
+            }
+          }
+        }
+      }
+
+      newArr[i][j] = count;
+      count = 0;
+    }
+  }
+
+  return newArr;
 }
 
 module.exports = {
