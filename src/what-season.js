673c6d8bd9f29526adcc3b6edf2c4738cdc2798d
boburwebdev <boburjon.zokirov.2017@gmail.com>
2025-03-03 03:47:06 +0500

    refactor: modify what-season to pass tricky tests

--- src/what-season.js
+++ src/what-season.js
@@ -14,30 +14,33 @@ const { NotImplementedError } = require('../extensions/index.js');
 function getSeason(date) {
   if (!date) return "Unable to determine the time of year!";
 
-  if (typeof date === "number" || new Date(date).toString() === "Invalid Date" || isNaN(new Date(date).valueOf()))  {
+  try {
+    date.getYear();
+
+    const month = date.getMonth();
+
+    switch (month) {
+      case 0:
+      case 1:
+      case 11:
+        return "winter";
+      case 2:
+      case 3:
+      case 4:
+        return "spring";
+      case 5:
+      case 6:
+      case 7:
+        return "summer";
+      case 8:
+      case 9:
+      case 10:
+        return "autumn";
+    }
+  } catch (err) {
     throw new Error("Invalid date!");
-  };
-
-  const month = date.getMonth();
-
-  switch (month) {
-    case 0:
-    case 1:
-    case 11:
-      return "winter";
-    case 2:
-    case 3:
-    case 4:
-      return "spring";
-    case 5:
-    case 6:
-    case 7:
-      return "summer";
-    case 8:
-    case 9:
-    case 10:
-      return "autumn";
   }
+
 }
 
 module.exports = {
