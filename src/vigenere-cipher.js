383886208c71b5e5d42acd44fd380027e290f4b7
boburwebdev <boburjon.zokirov.2017@gmail.com>
2025-03-03 04:10:01 +0500

    feat: add solution to vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,14 +19,71 @@ const { NotImplementedError } = require('../extensions/index.js');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
+const LATIN_LETTERS = "abcdefghijklmnopqrstuvwxyz";
+
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isDirect = true) {
+    this._isDirect = isDirect;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error("Incorrect arguments!")
+    }
+
+    let encryptedMessage = "";
+    let keyIndex = 0;
+
+    for (let i = 0; i < message.length; i++) {
+      const letterIndex = LATIN_LETTERS.indexOf(message[i].toLowerCase());
+      const keyChar = key[keyIndex % key.length];
+      const keyLetterIndex = LATIN_LETTERS.indexOf(keyChar.toLowerCase());
+
+      if (letterIndex === -1) {
+        encryptedMessage += message[i];
+      } else {
+        let encryptedLetterIndex = letterIndex + keyLetterIndex;
+        if (encryptedLetterIndex > 25) {
+          encryptedLetterIndex = encryptedLetterIndex - 26;
+        }
+
+        encryptedMessage += LATIN_LETTERS[encryptedLetterIndex];
+        keyIndex++;
+      }
+    }
+
+    return encryptedMessage.toUpperCase();
+  }
+
+  decrypt(encryptedMessage, key) {
+    if (!encryptedMessage || !key) {
+      throw new Error("Incorrect arguments!")
+    }
+
+    let decryptedMessage = "";
+    let keyIndex = 0;
+
+    for (let i = 0; i < encryptedMessage.length; i++) {
+      const letterIndex = LATIN_LETTERS.indexOf(encryptedMessage[i].toLowerCase());
+      const keyChar = key[keyIndex % key.length];
+      const keyLetterIndex = LATIN_LETTERS.indexOf(keyChar.toLowerCase());
+
+      if (letterIndex === -1) {
+        decryptedMessage += encryptedMessage[i];
+      } else {
+        let encryptedLetterIndex = letterIndex - keyLetterIndex + 26;
+        if (encryptedLetterIndex < 0) {
+          encryptedLetterIndex = encryptedLetterIndex + 25;
+        } else if (encryptedLetterIndex > 25) {
+          encryptedLetterIndex = encryptedLetterIndex - 26;
+        }
+
+        decryptedMessage += LATIN_LETTERS[encryptedLetterIndex];
+        keyIndex++;
+      }
+    }
+
+    return decryptedMessage.toUpperCase();
   }
 }
 
